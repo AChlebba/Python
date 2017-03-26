00ec703fe4c693d34eb54fa9be5d6d66124bf5f4
AChlebba <AChlebba@univ.gda.pl>
2017-03-26 10:22:45 +0200

    lab4

--- lab4/Towary.py
+++ lab4/Towary.py
@@ -13,12 +13,5 @@ def ListaCen(ceny, towary):
     for i in range(len(towary)):
         wynik['Produkt %d '%i + towary[i]] = ceny[towary[i]]
 
-    koszt = {}
-    koszt = wynik.keys()
-    cena = 0
-
-    for j in range(len(koszt)):
-        cena += koszt[j]
-
     return wynik
 print ListaCen(ceny2, towary)
